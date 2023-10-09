e5565f81f01fd559ea0c94d560181a4a24e6ab99
TAIJULAMAN <taijulaman8@gmail.com>
2023-10-09 22:52:22 +0600

    l

--- src/Components/CustomHook/useAxiosSecure.jsx
+++ src/Components/CustomHook/useAxiosSecure.jsx
@@ -5,7 +5,7 @@ import useAuth from './useAuth';
 
 
 const axiosSecure = axios.create({
-  baseURL: 'http://localhost:5000', 
+  baseURL: 'https://sodium-cafe-server-gg.vercel.app', 
 });
 
 const useAxiosSecure = () => {
